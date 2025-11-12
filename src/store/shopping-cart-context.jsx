9e0239bd60f18c596f7631371e714a59e18f41a6
Alicelle <529915006@qq.com>
2025-11-12 15:02:48 +0800

    first commit

---
+++ src/store/shopping-cart-context.jsx
@@ -0,0 +1,91 @@
+import { createContext,useState,useReducer } from "react";
+import { DUMMY_PRODUCTS } from '../dummy-products.js';
+export const CartContext = createContext({
+  items:[],
+  handleAddItemToCart:()=>{},
+  handleUpdateCartItemQuantity:() =>{}
+})
+const shoppingCartReducer = (state,action) =>{
+   if (action.type === 'ADD_CART') {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload.id
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload.id);
+      updatedItems.push({
+        id: action.payload.id,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+    return {
+      ...state,
+      items: updatedItems,
+    };
+   }
+   if (action.type === 'UPDATE_CART') {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+   }
+}
+const CartContextProvider = ({children}) =>{
+  const [shoppingCartState, ShoppingCartDispatch] = useReducer(shoppingCartReducer,{
+    items:[]
+  })
+ 
+  
+  function handleAddItemToCart(id) {
+    ShoppingCartDispatch({
+      type: 'ADD_CART',
+      payload: {
+        id
+      }
+    })
+    
+  }
+  function handleUpdateCartItemQuantity(productId, amount) {
+    ShoppingCartDispatch({
+      type: 'UPDATE_CART',
+      payload: {
+        productId,
+        amount
+      }
+    })
+  }
+  const ctxValue = {
+    items:shoppingCartState.items,
+    handleAddItemToCart,
+    handleUpdateCartItemQuantity
+  }
+  return < CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+}
+export default CartContextProvider
